b8b7698f55c643a0e97695f21c9f4ca785bc8375
Kagiso MAKO <kamako@c4r22s2.wethinkcode.co.za>
2019-10-13 17:02:31 +0200

    ex03 completed

---
+++ day03/ex03/cookie_crisp.php
@@ -0,0 +1,16 @@
+<?php
+    if ($_GET["action"] === "set")
+    {
+        setcookie($_GET["name"], $_GET["value"], time() + (86400 * 30));
+        echo "cookie created\n";   
+    }
+	else if ($_GET["action"] === "get")
+	{	
+		if ($_GET["name"] != FALSE)
+			echo $_GET["value"]."\n";
+	}
+	else if ($_GET["action"] === "del"){
+        setcookie($_GET["name"], "delete", 1);
+        echo "cookie deleted\n";   
+    }
+?>
